8957d1e01171482508fa2cab7c063e1ddcb8a300
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2025-04-04 06:26:01 +0700

    fix: missing props

--- src/utils/validateAtlasBtcDeposits.js
+++ src/utils/validateAtlasBtcDeposits.js
@@ -66,7 +66,6 @@ async function ValidateAtlasBtcDeposits(
             btcMempoolTxn,
             btcAtlasDepositAddress
           );
-       
 
           let btcStatus = 0;
           if (btcMempoolTxn.status.confirmed) {
@@ -92,6 +91,7 @@ async function ValidateAtlasBtcDeposits(
             yield_provider_txn_hash: "",
             retry_count: 0, // this field not used in validation
             minted_txn_hash_verified_count: 0, // this field not used in validation
+            custody_txn_id: "",
           };
           console.log(btcMempoolDepositRecord);
 
@@ -185,7 +185,9 @@ async function ValidateAtlasBtcDepositsMintedTxnHash(deposits, near) {
           }
         } else if (chainConfig.networkType === NETWORK_TYPE.NEAR) {
           try {
-            console.log(`Validating NEAR transaction: ${deposit.minted_txn_hash}`);
+            console.log(
+              `Validating NEAR transaction: ${deposit.minted_txn_hash}`
+            );
             const txResult = await near.provider.txStatus(
               deposit.minted_txn_hash,
               near.contract_id
@@ -219,7 +221,7 @@ async function ValidateAtlasBtcDepositsMintedTxnHash(deposits, near) {
                 const btcTxnHash = memo.btc_txn_hash;
 
                 if (btcTxnHash === deposit.btc_txn_hash) {
-                  const transactionHashValidated = 
+                  const transactionHashValidated =
                     await near.incrementDepositMintedTxnHashVerifiedCount(
                       deposit.btc_txn_hash,
                       deposit.minted_txn_hash
